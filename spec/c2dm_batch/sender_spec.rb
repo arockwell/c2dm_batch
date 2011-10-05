28a692483b2cdc52af2cc9fa34e62e1a2c7d6a22
Alex Rockwell <arockwell@gmail.com>
2011-10-05 11:18:43 -0700

    Decreased number of test notifications.

--- spec/c2dm_batch/sender_spec.rb
+++ spec/c2dm_batch/sender_spec.rb
@@ -20,12 +20,12 @@ describe C2dmBatch::Sender do
     notification = {
       :registration_id => @reg_id,
       :data => { 
-        :alert => "5 NFL Players Who Won't Replicate Last Year's Success",
+        :alert => "1 NFL Players Who Won't Replicate Last Year's Success",
         :url => "/articles/816975-nfl-5-players-who-wont-replicate-last-years-success",
         :tag => "boston-college-football"
       }
     }
-    #sender.send_notification(notification)
+    sender.send_notification(notification)
   end
 
   it "should send notifications in batches" do
@@ -35,7 +35,7 @@ describe C2dmBatch::Sender do
         :registration_id => @reg_id,
         :collapse_key => "1",
         :data => { 
-          :alert => "1 NFL Players Who Won't Replicate Last Year's Success",
+          :alert => "2 NFL Players Who Won't Replicate Last Year's Success",
           :url => "/articles/816975-nfl-5-players-who-wont-replicate-last-years-success",
           :tag => "buffalo-bills"
         }
@@ -44,7 +44,7 @@ describe C2dmBatch::Sender do
         :registration_id => @reg_id,
         :collapse_key => "2",
         :data => { 
-          :alert => "2 NFL Players Who Won't Replicate Last Year's Success",
+          :alert => "3 NFL Players Who Won't Replicate Last Year's Success",
           :url => "/articles/816975-nfl-5-players-who-wont-replicate-last-years-success",
           :tag => "miami-dolphins"
         }
@@ -53,63 +53,9 @@ describe C2dmBatch::Sender do
         :registration_id => @reg_id,
         :collapse_key => "3",
         :data => { 
-          :alert => "3 NFL Players Who Won't Replicate Last Year's Success",
-          :url => "/articles/816975-nfl-5-players-who-wont-replicate-last-years-success",
-          :tag => "new-york-jets"
-        }
-      },
-      {
-        :registration_id => @reg_id,
-        :collapse_key => "4",
-        :data => { 
           :alert => "4 NFL Players Who Won't Replicate Last Year's Success",
           :url => "/articles/816975-nfl-5-players-who-wont-replicate-last-years-success",
-          :tag => "baltimore-ravens"
-        }
-      },
-      {
-        :registration_id => @reg_id,
-        :collapse_key => "5",
-        :data => { 
-          :alert => "5 NFL Players Who Won't Replicate Last Year's Success",
-          :url => "/articles/816975-nfl-5-players-who-wont-replicate-last-years-success",
-          :tag => "cincinnati-bengals"
-        }
-      },
-      {
-        :registration_id => @reg_id,
-        :collapse_key => "6",
-        :data => { 
-          :alert => "6 NFL Players Who Won't Replicate Last Year's Success",
-          :url => "/articles/816975-nfl-5-players-who-wont-replicate-last-years-success",
-          :tag => "cleveland-browns"
-        }
-      },
-      {
-        :registration_id => @reg_id,
-        :collapse_key => "7",
-        :data => { 
-          :alert => "7 NFL Players Who Won't Replicate Last Year's Success",
-          :url => "/articles/816975-nfl-5-players-who-wont-replicate-last-years-success",
-          :tag => "pittsburgh-steelers"
-        }
-      },
-      {
-        :registration_id => @reg_id,
-        :collapse_key => "8",
-        :data => { 
-          :alert => "8 NFL Players Who Won't Replicate Last Year's Success",
-          :url => "/articles/816975-nfl-5-players-who-wont-replicate-last-years-success",
-          :tag => "houston-texans"
-        }
-      },
-      {
-        :registration_id => @reg_id,
-        :collapse_key => "9",
-        :data => { 
-          :alert => "9 NFL Players Who Won't Replicate Last Year's Success",
-          :url => "/articles/816975-nfl-5-players-who-wont-replicate-last-years-success",
-          :tag => "indianapolis-colts"
+          :tag => "new-york-jets"
         }
       }
     ]
